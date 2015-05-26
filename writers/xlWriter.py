3cc041f80aa5899375b2e46ed9e039148a5c122d
golanb7108 <golan3873@gmail.com>
2015-05-27 00:37:01 +0300

    xl-my-expenses: fixing prior commit

---
+++ writers/xlWriter.py
@@ -0,0 +1,30 @@
+__author__ = 'gbenami'
+
+"""
+xl-my-expenses - A utility for parsing a bank sheet into
+your expenses excel.
+xl Writer is an interface of different xl writers.
+"""
+
+from openpyxl import load_workbook
+from openpyxl import workbook
+
+class xlWriter(object):
+    def __init__(self, fileName, writerType):
+        """
+        Opens the input file/stream and gets ready to parse it.
+        """
+        self.fileName = fileName
+        self.writerType = writerType
+        self.wb = load_workbook(filename = self.fileName, read_only=False)
+
+    def writeRecord(self, record):
+        """
+        Checks if there is more records, and sets the current record
+        to be the next record.
+        :return: True if there are.
+        """
+        raise NotImplementedError( "xlWriter is an abstract class" )
+
+    def __str__(self):
+        return "%s writer is writing to file %s." % (self.writerType, self.fileName)
