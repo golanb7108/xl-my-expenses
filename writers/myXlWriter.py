47bcf3ed0ff4cb0d033009da5e6c30e9d06a1b7b
golanb7108 <golan3873@gmail.com>
2015-05-27 21:03:11 +0300

    xl-my-expenses: Add many functionalities.
    1. Add finding the empty row in the destination file.
    2. Add writing the record to the file.

--- writers/myXlWriter.py
+++ writers/myXlWriter.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 __author__ = 'gbenami'
 
 """
@@ -9,6 +10,7 @@ my Xl Writer is an instance of XL writer.
 from openpyxl import load_workbook
 from xlWriter import xlWriter
 from openpyxl import workbook
+import constants
 
 class myXlWriter(xlWriter):
     def __init__(self, fileName, writerType):
@@ -16,7 +18,54 @@ class myXlWriter(xlWriter):
         Opens the input file/stream and gets ready to parse it.
         """
         xlWriter.__init__(self, fileName, writerType)
-        # calculate here the first free line for writing
+        self.emptyRowsPerSheet = {}
+
+    def _findFreeRow(self, record):
+        """
+        Find free row to write records to.
+        """
+        rowsToIter = None
+        expenseStartColumn = ''
+        expenseStartRow = ''
+        expenseEndColumn = ''
+        expenseEndRow = ''
+        sheetName = constants.MONTH_NUM_TO_STRING[record.getDate().tm_mon]
+        if self.emptyRowsPerSheet[sheetName]:
+           self.emptyRowsPerSheet[sheetName][0] += 1
+        else:
+            self.ws = self.wb.get_sheet_by_name(sheetName)
+            recordsSignature = (constants.DATE_SIG, constants.CATEGORY_SIG,
+                                constants.DESCRIPTION_SIG, constants.COST_SIG)
+
+            for row in self.ws.iter_rows(row_offset=1):
+                for cell in row:
+                    rowTuple = (row[ord(cell.column) - 96 + i] for i in range(4))
+                    if rowTuple == recordsSignature:
+                        expenseStartColumn = str(cell.column)
+                        expenseStartRow = str(cell.row)
+                        expenseEndColumn = str(chr(ord(cell.column.lower()) + 3)).upper()
+                        expenseEndRow = str(cell.row + 56)
+                        rowsToIter = "%s%s:%s%s" % (expenseStartColumn, expenseStartRow,
+                                                    expenseEndColumn, expenseEndRow)
+
+            if rowsToIter == None:
+                raise ValueError('The sheet does not have an expenses table.')
+
+            wsRows = [row for row in self.ws.iter_rows(rowsToIter)]
+            foundEmptyLine = False
+            emptyRow = -1
+            for rowNumber, line in enumerate(wsRows):
+                currentRow = (line[i].value for i in range(4))
+                if all(item is None for item in currentRow) and not foundEmptyLine:
+                    foundEmptyLine = True
+                    emptyRow = rowNumber
+                elif not all(item is None for item in currentRow) and foundEmptyLine:
+                    foundEmptyLine = False
+            firstEmptyRow = emptyRow + int(expenseStartRow)
+
+            self.emptyRowsPerSheet[sheetName] = (firstEmptyRow, expenseStartColumn)
+
+        return self.emptyRowsPerSheet[sheetName]
 
     def writeRecord(self, record):
         """
@@ -24,5 +73,11 @@ class myXlWriter(xlWriter):
         to be the next record.
         :return: True if there are.
         """
-        # TODO - need to be implemented
-        pass
+        (emptyRow, emptyColumn) = self._findFreeRow(record)
+        dateSlot = "%s%s" % (str(emptyColumn), str(emptyRow))
+        collectorSlot = "%s%s" % (str(chr(ord(emptyColumn.lower()) + 2)).upper(), str(emptyRow))
+        costSlot = "%s%s" % (str(chr(ord(emptyColumn.lower()) + 3)).upper(), str(emptyRow))
+
+        self.ws[dateSlot] = record.getDate()
+        self.ws[collectorSlot] = record.getCollector()
+        self.ws[costSlot] = record.getCost()
