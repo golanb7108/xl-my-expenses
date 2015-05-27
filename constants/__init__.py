e1a98792f5145c70d9c87ce4ea0c40f2ddcca55d
golanb7108 <golan3873@gmail.com>
2015-05-27 20:57:45 +0300

    xl-my-expenses: Add constants file
    Add constants file to project in order to maintain all of the constants in one place.

---
+++ constants/__init__.py
@@ -0,0 +1,9 @@
+__author__ = 'gbenami'
+
+"""
+xl-my-expenses - A utility for parsing a bank sheet into
+your expenses excel.
+__init__.py is an importing script.
+"""
+
+__all__ = ['constants']
---
+++ constants/constants.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+__author__ = 'gbenami'
+
+"""
+xl-my-expenses - A utility for parsing a bank sheet into
+your expenses excel.
+constants.py holds all of the constants in the project.
+"""
+
+DATE_SIG = u"תאריך"
+CATEGORY_SIG = u"קטגוריה"
+DESCRIPTION_SIG = u"תיאור"
+COST_SIG = u"סכום"
+MONTH_NUM_TO_STRING = {1: u'ינואר', 2: u'פברואר', 3: u'מרץ', 4: u'אפריל', 5: u'מאי', 6: u'יוני', 7: u'יולי',
+                       8: u'אוגוסט', 9: u'ספטמבר', 10: u'אוקטובר', 11: u'נובמבר', 12: u'דצמבר'}
