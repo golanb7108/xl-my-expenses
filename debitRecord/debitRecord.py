1066ac62d8ee59514f5597554e66b990686e343e
golanb7108 <golan3873@gmail.com>
2015-05-27 20:22:43 +0300

    xl-my-expenses: Format time to dates
    Format time in the record object in to date objects.

--- debitRecord/debitRecord.py
+++ debitRecord/debitRecord.py
@@ -5,13 +5,22 @@ xl-my-expenses - A utility for parsing a bank sheet into
 your expenses excel.
 Debit Record represents a debit credit card record.
 """
+import time
+import re
 
 class debitRecord(object):
     def __init__(self, date, collector, cost):
         """
         Opens the input file/stream and gets ready to parse it.
         """
-        self.date = date
+        datePatt = [re.compile("(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})"),
+                    re.compile("(\d{2})/(\d{2})/(\d{4})")]
+        if datePatt[0].match(date):
+            fmt_date = time.strptime(date, "%Y-%d-%m %H:%M:%S")
+        elif datePatt[1].match(date):
+            fmt_date = time.strptime(date, "%d/%m/%Y")
+
+        self.date = fmt_date
         self.collector = collector
         self.cost = cost
 
