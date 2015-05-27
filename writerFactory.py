56eaad67c318eebb17ee940e5b16f93ea2c5ab51
golanb7108 <golan3873@gmail.com>
2015-05-27 21:06:34 +0300

    xl-my-expenses: Add writer factory
    Add a writer factory which returns the corresponding writer to the xl type.

---
+++ writerFactory.py
@@ -0,0 +1,15 @@
+__author__ = 'gbenami'
+
+"""
+xl-my-expenses - A utility for parsing a bank sheet into
+your expenses excel.
+Writer factory is a factory for xl writers.
+"""
+from writers import *
+
+class writerFactory(object):
+    # Create based on parser name:
+    def createWriter(type, fileName):
+        if type == "my": return myXlWriter(fileName, type)
+    writerFactory = staticmethod(writerFactory)
+
