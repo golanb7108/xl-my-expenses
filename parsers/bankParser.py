7b3eb4cc9babc032417e1d940b5c0defad5b1767
golanb7108 <golan3873@gmail.com>
2015-05-29 15:07:09 +0300

    xl-my-expenses: Parser: Fix design, move common functionality to Father
    Move all of the functionality possible from the sons to father.

--- parsers/bankParser.py
+++ parsers/bankParser.py
@@ -7,6 +7,7 @@ Bank Parser is an interface of different bank parsers.
 """
 
 from openpyxl import load_workbook
+import re
 
 class bankParser(object):
     def __init__(self, fileName, bankName):
@@ -16,6 +17,18 @@ class bankParser(object):
         self.fileName = fileName
         self.bankName = bankName
         self.wb = load_workbook(filename = self.fileName, read_only=False)
+        first_sheet = self.wb.get_sheet_names()[0]
+        self.ws = self.wb.get_sheet_by_name(first_sheet)
+        self.currentRow = 0
+        self.currentRecord = 0
+        self.wsRows = []
+        self.recordsList = []
+        self.datePatt = [re.compile("(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})"),
+                         re.compile("(\d{2})/(\d{2})/(\d{4})")]
+
+        # Get all rows
+        rowsToIter = "A1:G" + str(self.ws.get_highest_row())
+        self.wsRows = [row for row in self.ws.iter_rows(rowsToIter)]
 
     def hasMoreRecords(self):
         """
@@ -30,28 +43,28 @@ class bankParser(object):
         Get the current record.
         :return: the current record that is pointed.
         """
-        raise NotImplementedError( "bankParser is an abstract class" )
+        return self.currentRecord
 
     def getDate(self):
         """
         Get the date of the expense.
         :return: return the date.
         """
-        raise NotImplementedError( "bankParser is an abstract class" )
+        return self.currentRecord.getDate()
 
     def getCost(self):
         """
         Get the cost of the current record.
         :return: the cost.
         """
-        raise NotImplementedError( "bankParser is an abstract class" )
+        return self.currentRecord.getCost()
 
     def getCollector(self):
         """
         Get the collector of the current record.
         :return: the Collector.
         """
-        raise NotImplementedError( "bankParser is an abstract class" )
+        return self.currentRecord.getCosllector()
 
     def __str__(self):
         return "%s parser is parsing file %s." % (self.bankName, self.fileName)
--- parsers/hapoalimCreditParser.py
+++ parsers/hapoalimCreditParser.py
@@ -6,10 +6,8 @@ your expenses excel.
 HaPoalim Credit Parser is an instance of a bank parser for hapoalim.
 """
 
-from openpyxl import load_workbook
 from bankParser import bankParser
 from debitRecord import debitRecord
-import re
 
 class hapoalimCreditParser(bankParser):
     def __init__(self, fileName, bankName):
@@ -17,18 +15,6 @@ class hapoalimCreditParser(bankParser):
         Opens the input file/stream and gets ready to parse it.
         """
         bankParser.__init__(self, fileName, bankName)
-        first_sheet = self.wb.get_sheet_names()[0]
-        self.ws = self.wb.get_sheet_by_name(first_sheet)
-        self.currentRow = 0
-        self.currentRecord = 0
-        self.wsRows = []
-        self.recordsList = []
-        self.datePatt = [re.compile("(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})"),
-                         re.compile("(\d{2})/(\d{2})/(\d{4})")]
-
-        # Get all rows
-        rowsToIter = "A1:G" + str(self.ws.get_highest_row())
-        self.wsRows = [row for row in self.ws.iter_rows(rowsToIter)]
 
     def hasMoreRecords(self):
         """
@@ -53,33 +39,3 @@ class hapoalimCreditParser(bankParser):
                 pass
         # In case no more rows or no more records
         return False
-
-    def getRecord(self):
-        """
-        Get the current record.
-        :return: the current record that is pointed.
-        """
-        return self.currentRecord
-
-    def getDate(self):
-        """
-        Get the date of the expense.
-        :return: return the date.
-        """
-        return self.currentRecord.getDate()
-
-    def getCost(self):
-        """
-        Get the cost of the current record.
-        :return: the cost.
-        """
-        return self.currentRecord.getCost()
-
-    def getCollector(self):
-        """
-        Get the collector of the current record.
-        :return: the Collector.
-        """
-        return self.currentRecord.getCollector()
-
-
